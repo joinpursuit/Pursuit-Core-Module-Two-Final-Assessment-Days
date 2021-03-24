ad249a0a5dc8df4ad60efe9d16695ec056f7d8e8
Jon Narine <jonnicwolf@gmail.com>
2021-03-24 15:59:25 -0400

    replace options.value by index instead as title key

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 //h3 p1 should have the year of the movie name
 //h3 p2 should have the movie description
 const movies = document.querySelector('#movies')
+const button = document.querySelector('#movie-input')
+const movieInput = document.querySelector('#movie-input')
 const movieName = document.querySelector('h3')
 const year = document.querySelector('#year')
 const description = document.querySelector('#description')
-
 
 const movieGrab = () => {
     axios.get('https://ghibliapi.herokuapp.com/films')
@@ -17,10 +18,17 @@ const movieGrab = () => {
             const options = document.createElement('option')
             movies.appendChild(options)
             options.textContent = array[i].title
-            options.value = i
+            options.value = array[i].title
         }
-        console.log(movies)
-        
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+            //this gives me all of the names except for one
+            console.log(movies.value)
+            //match by value=n 
+            //if n
+            
+            
+        })
     })
     .catch(err => (err))
 }
