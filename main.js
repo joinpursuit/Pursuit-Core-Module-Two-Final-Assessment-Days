67df22ab9db154813d4220d972e484405f243d5f
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-03-24 13:57:37 -0400

    movie option appearing ... at least one is so far ...

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const movieTitles = document.querySelector('select');
 
 
+// !Select feature
 async function ghibliFilms() {
     const movieResults = await axios.get("https://ghibliapi.herokuapp.com/films")
 
@@ -16,7 +17,25 @@ async function ghibliFilms() {
         const titleNames = document.createElement('option')
         titleNames.textContent = ghibliTitle.title
         movieTitles.appendChild(titleNames)
+
+        const movieSelectOptions = document.querySelector('#movie-titles')
+
+        movieSelectOptions.addEventListener('change', (e) => {
+            e.preventDefault()
+
+
+            const mt = document.querySelector('#display-info h3')
+            const movieYear = document.querySelector('#display-info p:first-of-type')
+            const describe = document.querySelector('#display-info p:last-of-type')
+
+            mt.textContent = ghibliTitle.title
+            movieYear.textContent = ghibliTitle.release_date
+            describe.textContent = ghibliTitle.description
+
+        })
     }
+    // !End of Select Feature
+
 
 }
 ghibliFilms();
