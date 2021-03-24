0fdf8ddbf502702deac1c78a36af640df1f86037
cassidybeni <cassidybeni@pursuit.org>
2021-03-24 15:26:07 -0400

    Pass 16/18 tests

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-//goes in section
-let title = document.createElement("h3")
-let release  = document.createElement("p")
-let description  = document.createElement("p")
+let select = document.querySelector("select")
+let review = document.querySelector("#text")
+let section = document.querySelector("#display-info")
+let form = document.querySelector("form")
+let ul = document.querySelector("ul")
+
+const getFilms = async () => {
+    try {
+        let res = await axios.get(`https://ghibliapi.herokuapp.com/films`)
+        res.data.forEach((el) => {
+            let option = document.createElement("option")
+            option.value = el.id
+            option.text = el.title
+            select.appendChild(option)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+getFilms()
+
+select.addEventListener("change", async (event) => {
+    
+    try {
+        let res = await axios.get(`https://ghibliapi.herokuapp.com/films/${event.target.value}`)
+
+        let title = document.createElement("h3")
+        let release  = document.createElement("p")
+        let description  = document.createElement("p")
+
+        title.innerText = res.data.title
+        release.innerText = res.data.release_date
+        description.innerText = res.data.description
+    
+        section.innerText = ""
+        
+        section.appendChild(title)
+        section.appendChild(release)
+        section.appendChild(description)
+    } catch (error) {
+        
+    }
+})
+
+form.addEventListener("submit", async event => {
+    event.preventDefault()
+
+    let title = document.querySelector("h3")
+    let li = document.createElement("li")
+    li.innerHTML = `<strong>${title.innerText}.</strong> ${review.value}`
+    ul.appendChild(li)
+    review.value = ""
+})
