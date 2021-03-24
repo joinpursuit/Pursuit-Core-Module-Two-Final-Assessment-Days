51ac6ab0b1a1ac79b9ef740ddbae635e287f5f8f
Talia Pichardo <taliapichardo@gmail.com>
2021-03-24 16:46:56 -0400

    comment list

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const selectOption = document.querySelector("select");
-const submitBtn = document.querySelector("#submit")
-const filmTitle = document.querySelector("h3");
-const filmYear = document.querySelector("#release-year");
-const filmDescription = document.querySelector("#description");
+const submitBtn = document.querySelector("#submit");
+const theForm = document.querySelector("form");
+const commentUl = document.querySelector("ul");
+
 
 async function getFilms(){
     const res = await axios.get("https://ghibliapi.herokuapp.com/films")
     const getResult = res.data
-    console.log(getResult)
-    for(let film of getResult){
+   
+    getResult.forEach(film => {
         const item = addOptions(film)
         selectOption.appendChild(item)
-    }
+        
+    });
 }
 
 function addOptions(film){
     const filmOptions = document.createElement("option");
     filmOptions.text = film.title
-    filmOptions.value = film.title
     
     selectOption.addEventListener("change", ()=>{
-        function selectOptions(){
-            filmTitle.textContent = selectOption.value
-            filmYear.textContent = film.release_date
-            filmDescription.textContent = film.description
-        }
         selectOptions()
     });
+    
+    function selectOptions(){
+        const filmTitle = document.querySelector("#display-info h3");
+        const filmYear = document.querySelector("#display-info p:first-of-type");
+        const filmDescription = document.querySelector("#display-info p:last-of-type");
+        filmTitle.textContent = selectOption.value
+        filmYear.textContent = film.release_date
+        filmDescription.textContent = film.description
+    }
+
     
     return filmOptions
 }
 
 
+theForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const textInput = document.querySelector("#text").value;
+    const commentLi = document.createElement("li");
+    commentLi.textContent = `${textInput}`
+    commentUl.appendChild(commentLi)
+})
 
 
 
