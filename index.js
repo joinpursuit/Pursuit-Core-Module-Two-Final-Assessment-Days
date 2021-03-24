9ce0ce69cbb46524b59ccdb3af4bfa1a09446148
Jon Narine <jonnicwolf@gmail.com>
2021-03-24 12:56:55 -0400

    select menu sees options append

---
+++ index.js
@@ -0,0 +1,39 @@
+//create option tags and append them to select tag
+//option tags should contain movie name
+//selecting movie name should fill h3 
+//h3 p1 should have the year of the movie name
+//h3 p2 should have the movie description
+const movies = document.querySelector('#movies')
+const movieName = document.querySelector('h3')
+const year = document.querySelector('#year')
+const description = document.querySelector('#description')
+
+
+const movieGrab = () => {
+    axios.get('https://ghibliapi.herokuapp.com/films')
+    .then(response => {
+        const array = response.data
+        for (let i = 0; i < array.length; i++) {            
+            const options = document.createElement('option')
+            movies.appendChild(options)
+            options.textContent = array[i].title
+            // console.log(options)
+            
+            
+        }        
+    }).catch(err =>(err))
+}
+
+movieGrab()
+
+
+
+
+
+
+
+
+
+
+
+
