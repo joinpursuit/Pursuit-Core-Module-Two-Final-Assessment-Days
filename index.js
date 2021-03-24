9cc41950accd504cdb980c002b6724352727f298
Talia Pichardo <taliapichardo@gmail.com>
2021-03-24 18:16:48 -0400

    final commit

--- index.js
+++ index.js
@@ -2,51 +2,43 @@ const selectOption = document.querySelector("select");
 const submitBtn = document.querySelector("#submit");
 const theForm = document.querySelector("form");
 const commentUl = document.querySelector("ul");
+const filmTitle = document.querySelector("h3");
+const filmYear = document.querySelector("p:first-of-type");
+const filmDescription = document.querySelector("p:last-of-type");
 
 
 async function getFilms(){
     const res = await axios.get("https://ghibliapi.herokuapp.com/films")
     const getResult = res.data
-   
+    
     getResult.forEach(film => {
-        const item = addOptions(film)
-        selectOption.appendChild(item)
-        
+        const filmOptions = document.createElement("option");
+        filmOptions.textContent = film.title
+        filmOptions.value = film.id
+        selectOption.appendChild(filmOptions)
     });
 }
 
-function addOptions(film){
-    const filmOptions = document.createElement("option");
-    filmOptions.text = film.title
+    selectOption.addEventListener("change", async (event)=>{
+        const res = await axios.get(`https://ghibliapi.herokuapp.com/films/${event.target.value}`)
+        filmTitle.textContent = res.data.title
+        filmYear.textContent = res.data.release_date
+        filmDescription.textContent = res.data.description
     
-    selectOption.addEventListener("change", ()=>{
-        selectOptions()
     });
     
-    function selectOptions(){
-        const filmTitle = document.querySelector("#display-info h3");
-        const filmYear = document.querySelector("#display-info p:first-of-type");
-        const filmDescription = document.querySelector("#display-info p:last-of-type");
-        filmTitle.textContent = selectOption.value
-        filmYear.textContent = film.release_date
-        filmDescription.textContent = film.description
-    }
-
-    
-    return filmOptions
-}
-
 
 theForm.addEventListener("submit", (e)=>{
     e.preventDefault()
     const textInput = document.querySelector("#text").value;
     const commentLi = document.createElement("li");
-    commentLi.textContent = `${textInput}`
+    commentLi.innerHTML = `<strong>${filmTitle.textContent}</strong>: ${textInput}`
     commentUl.appendChild(commentLi)
 })
 
 
-
+// li.innerHTML = `${title.textContent}: ${textInput.value}`;
 
 
 getFilms()
+
