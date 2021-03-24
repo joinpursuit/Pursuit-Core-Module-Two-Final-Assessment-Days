55c08597a48642ca60257efc250329296f161a11
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-03-24 16:53:32 -0400

    cant append smh

--- index.js
+++ index.js
@@ -1,55 +1,42 @@
-const allMovies = document.querySelector('#select-title')
-const form = document.querySelector('#review-box')
+const select = document.querySelector("select");
+const form = document.querySelector("form");
 const commentList = document.getElementById('review-comments')
+const titles = document.querySelector("#title");
+const year = document.querySelector("#release");
+const info = document.querySelector("#description");
+
+async function getFilms() {
+  const res = await axios.get("https://ghibliapi.herokuapp.com/films");
+  const apiResult = res.data;
+
+  apiResult.forEach((film) => {
+    const option = document.createElement("option");
+    option.textContent = film.title;
+    option.value = film.id;
+    select.appendChild(option);
+  });
+}
+
+select.addEventListener("change", (e) => {
+  fetch(`https://ghibliapi.herokuapp.com/films/${e.target.value}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((Movie) => {
+      titles.innerHTML = Movie.title;
+      year.innerHTML = Movie.release_date;
+      info.innerHTML = Movie.description;
+    });
+});
 
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
-
     const currTitle = document.querySelector('title').textContent
-
     const comment = document.querySelector('#review').value
     
     const li = document.createElement('li')
     li.innerHTML = `${comment}`
     commentList.appendChild(li)
-
 })
 
-async function getMovies () {
-    const res = await axios.get("https://ghibliapi.herokuapp.com/films")
-    const apiResult = res.data
-
-    apiResult.forEach(film => {
-        const movie = document.createElement("option")
-        allMovies.textContent = movie
-        allMovies.appendChild(movie)
-        console.log(film)
-
-    })
-
-}
-
-function chooseMovie(list) {
-    const li = document.createElement("select")
-
-    const option = document.createElement('li')
-    option.innerText = list.title
-    li.appendChild(option)
-
-
-    li.addEventListener("click", ()=> {
-        const title = document.querySelector('#movie-title')
-        const year = document.querySelector('#release-year')
-        const description = document.querySelector('#description')
-
-        title.textContent = film.data.title
-        year.textContent = movie.data.release_date
-        description.textContent = movie.data.description
-
-    })
-
-    return li
-}
-
-
-getMovies()
+getFilms();
