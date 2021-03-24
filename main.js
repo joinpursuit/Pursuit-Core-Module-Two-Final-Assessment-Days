a9887b30b2991befc645740e65e75e75dee542cc
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-03-24 13:06:49 -0400

    initial-specs.js passing

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const movieTitles = document.querySelector('#movie-titles');
+const movieTitles = document.querySelector('select');
 
 
 async function ghibliFilms() {
     const movieResults = await axios.get("https://ghibliapi.herokuapp.com/films")
 
     const allMovies = movieResults.data
-    console.log(allMovies.title)
+
+    for (let title of allMovies) {
+      filmData(title)
+    }
+
+
+
+    function filmData (ghibliTitle) {
+        const titleNames = document.createElement('option')
+        titleNames.textContent = ghibliTitle.title
+        movieTitles.appendChild(titleNames)
+    }
+
 }
 ghibliFilms();
