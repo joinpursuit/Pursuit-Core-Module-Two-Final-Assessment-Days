a9e4ddd4ffac035c609d25f6fd44cacb7d26433d
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-03-24 11:01:54 -0400

    set up html and css

---
+++ index.js
@@ -0,0 +1,7 @@
+//select box that contains the title of each movie in the API
+    //when change populate the section "#display-info"
+    //when change again repopulate with new info
+// in section
+    //create h3(title), and release year (p), and description (p), within the section "#display-info" of single film 
+//form allows users to submit (not save, just add to the frontend) a review of that film. On submission the input should clear.
+    //upon submission new li appended to the ul: title(bold): review
