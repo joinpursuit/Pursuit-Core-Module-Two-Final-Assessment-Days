a6c7e19f304fdf31810837dc05d49c3066f56728
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-03-24 20:58:35 -0400

    all test passing

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-//select box that contains the title of each movie in the API (done)
-//when change populate the section "#display-info"
-//when change again repopulate with new info
-//<option> element should have a value attribute containing the data value to submit to the server when that option is selected.the value defaults to the text contained inside the element.
-//option value stored
-
-// in section
-//form allows users to submit (not save, just add to the frontend) a review of that film. On submission the input should clear.
-//upon submission new li appended to the ul: title(bold): review
 
 const selectBox = document.querySelector("#filmTitles");
 
