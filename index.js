0be9e0e155d913b4c03f0b391e24d648276c3102
Jon Narine <jonnicwolf@gmail.com>
2021-03-28 02:17:50 -0400

    select works without sumbit button

--- index.js
+++ index.js
@@ -12,23 +12,35 @@ const movieGrab = () => {
     axios.get('https://ghibliapi.herokuapp.com/films')
     .then(response => {
         const array = response.data        
-        for (let i = 0; i < array.length; i++) {
-        
+        for (let i = 0; i < array.length; i++) {        
             const options = document.createElement('option')
             movies.appendChild(options)
             options.textContent = array[i].title
             options.value = array[i].title
         }
-        button.addEventListener('click', (e) => {
-            e.preventDefault()
+        movies.addEventListener('input', (e) => {
+            // e.preventDefault()
+            console.log('click seen')
+            console.log(movies.value)
+            
             movieName.textContent = movies.value
-            for (let i = 0; i < array.length; i++) {                
+            for (let i = 0; i < array.length; i++) {
                 if (movies.value === array[i].title) {
                     year.textContent = array[i].release_date
-                    description.textContent = array[i].description                    
+                    description.textContent = array[i].description
                 }
-            }            
+            }
         })
+        // button.addEventListener('click', (e) => {
+        //     e.preventDefault()
+        //     movieName.textContent = movies.value
+        //     for (let i = 0; i < array.length; i++) {                
+        //         if (movies.value === array[i].title) {
+        //             year.textContent = array[i].release_date
+        //             description.textContent = array[i].description                    
+        //         }
+        //     }            
+        // })
         reviewSubmit.addEventListener('click', (e) => {
             e.preventDefault()
             const li = document.createElement('li')            
