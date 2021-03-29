7027117bf851de91f87597060b78278f88ff3eea
Ivan Castillo <total180training@gmail.com>
2021-03-29 16:57:58 -0400

    changed how information to display in comments list is accessed, so that we have more access to other information

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    const displayInfo = document.querySelector('#display-info')
+
     async function getFilms() {
         const res = await axios.get('https://ghibliapi.herokuapp.com/films')
         const filmsArr = res.data
@@ -22,19 +24,18 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let film of newArr) {
             let title = film.title
             let option = document.createElement('option')
-            // option.value = film.id
             option.value = index
             option.textContent = title
             select.appendChild(option)
             index++
+            console.log(option)
         }
         const form = document.querySelector('form')
-        const textInput = form.firstElementChild
-        const btn = form.lastElementChild
+        const comment = form.firstElementChild
         const ul = document.querySelector('ul')
 
         select.addEventListener('change', (event) => {
-            let movie = filmsArr[event.target.value]
+            let movie = newArr[event.target.value]
             titleBox.textContent = movie.title
             descriptionBox.textContent = movie.description
             yearBox.textContent = movie.release_date
@@ -42,11 +43,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         form.addEventListener('submit', (event) => {
             event.preventDefault()
-            let title = document.querySelector('#display-info h3').textContent
-            console.log(title)
+            // let title = document.querySelector('#display-info h3').textContent
+            let val = select.value
+            console.log(val)
+            let movie = newArr[val]
+            console.log(movie)
             let li = document.createElement('li')
-            li.innerHTML = `<b>${title}:</b> ${textInput.value}`
+            // li.innerHTML = `<b>${title}:</b> ${textInput.value}`
+            li.innerHTML = `<b>${movie.title} - ${movie.original_title}:</b> ${comment.value}`
             ul.appendChild(li)
+
             form.reset()
         })
 
