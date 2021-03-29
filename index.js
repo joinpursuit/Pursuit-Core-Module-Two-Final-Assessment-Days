b43405062d8470a5a9c57d8e91b61d9957d76228
Angelina Ebreo <angelinaebreo@gmail.com>
2021-03-29 12:58:39 -0400

    revised code

--- index.js
+++ index.js
@@ -2,60 +2,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("#movie-review");
   const reviewList = document.querySelector("#movie-review-ul");
   const options = document.querySelector("select");
-  const choice = document.querySelector("#movie-titles");
+  // const choice = document.querySelector("#movie-titles");
   const title = document.querySelector("#title");
   const year = document.querySelector("#release-year");
   const description = document.querySelector("#description");
-
 
   const getTitles = async () => {
     try {
       response = await axios.get("https://ghibliapi.herokuapp.com/films");
       allMovies = response.data;
-   
-       
-       allMovies.forEach(movie => { 
-           console.log(allMovies)
-           const option = document.createElement("option");
+
+      allMovies.forEach((movie) => {
+        const option = document.createElement("option");
         option.textContent = movie.title;
         option.value = movie.id;
         options.appendChild(option);
-
-        
-       })
-        
-    
+      });
     } catch (err) {
       console.log("Achtung, we got an error", err);
     }
   };
 
   getTitles();
+  // solution with second API call
+  // options.addEventListener("change", (e) => {
+  //   console.log(e.target.value);
+  //   fetch(`https://ghibliapi.herokuapp.com/films/${e.target.value}`)
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw Error("Achtung, we got an error: ", Error);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((currentMovie) => {
+  //       title.textContent = currentMovie.title;
+  //       year.textContent = currentMovie.release_date;
+  //       description.textContent = currentMovie.description;
+  //     });
+  // });
 
-
+  //solution without second APi call
   options.addEventListener("change", (e) => {
-    console.log(e.target.value);
-  fetch(`https://ghibliapi.herokuapp.com/films/${e.target.value}`)
-  .then(response => {
-      if(!response.ok) {
-          throw Error("Achtung, we got an error: ", Error);
-      } return response.json()
-  })
-  .then(currentMovie => {
-      title.textContent = currentMovie.title
-      year.textContent = currentMovie.release_date
-      description.textContent = currentMovie.description
-
-  })
-
-
-
-
-  
+    title.textContent = allMovies[options.selectedIndex - 1].title;
+    year.textContent = allMovies[options.selectedIndex - 1].release_date;
+    description.textContent = allMovies[options.selectedIndex - 1].description;
   });
-
-
-
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -65,11 +56,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (review) {
       newReview.innerHTML = `<b>${title.textContent}: </b> ${review}`;
       reviewList.appendChild(newReview);
-      form.reset()
+      form.reset();
     } else {
       newReview.innerHTML = `<b>${title.textContent}: </b> This is the best movie I've ever seen`;
       reviewList.appendChild(newReview);
-     form.reset()
+      form.reset();
     }
   });
 });
