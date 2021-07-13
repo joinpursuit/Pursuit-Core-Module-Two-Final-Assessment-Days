295df614ae974178fbe864cd2bd247ad8b426383
Yesenia L <ylopezcr123@gmail.com>
2021-07-12 21:19:44 -0400

    updates

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-const allMovies = document.querySelector("#options")
-const title = document.querySelector('#mTitle')
-const releaseD = document.querySelector("#release-date")
-const descriptions = document.querySelector('#description')
-const form = document.querySelector("form")
-const reviews = document.querySelector("#user-reviews")
+const allMovies = document.querySelector("#options");
+const submit = document.querySelector("submit");
+const releaseD = document.querySelector("p:first-of-type");
+const descriptions = document.querySelector("p:last-of-type");
+const form = document.querySelector("form");
+const title = document.querySelector("h3");
+// const display = document.querySelector("#display-info");
 
 const getMovies = async () => {
-    try {
-        const res = await axios.get ("https://ghibliapi.herokuapp.com/films")
-        res.data.forEach(film => {
-            const choices = document.createElement("option")
-            choices.innerText = film.title
-            choices.value = film.id
-            allMovies.appendChild(choices)
-        })
-    } catch (err) {
-        console.log(err)
-    }
-}
-getMovies()
+  try {
+    const res = await axios.get("https://ghibliapi.herokuapp.com/films");
+    res.data.forEach((film) => {
+      const choices = document.createElement("option");
+      choices.textContent = film.title;
+      choices.value = film.id;
+      allMovies.appendChild(choices);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 allMovies.addEventListener("change", async (e) => {
-    try {
-        const url = `https://ghibliapi.herokuapp.com/films/${e.target.value}`
-        const res = await axios.get(url);
-        title.textContent = res.data.title
-        releaseD.textContent = res.data.release_date
-        descriptions.textContent = res.data.description
-
-    } catch (err) {
-        console.log(err)
-    }
-})
+  const res = await axios.get(`https://ghibliapi.herokuapp.com/films/${e.target.value}`);
+  // const res = await axios.get(url);
+  title.textContent = res.data.title;
+  releaseD.textContent = res.data.release_date;
+  descriptions.textContent = res.data.description;
+});
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const review= document.querySelector('#review')
-    const li = document.createElement('li')
-    li.innerHTML = `<b>${title.textContent}:<b> ${review.value}`
-    reviews.appendChild(li)
-    review.value = ""
-})
+  e.preventDefault();
+  const review = document.querySelector("input[type=text]").value;
+  const li = document.createElement("li");
+  const ul = document.querySelector("ul");
+  li.innerHTML = `<b>${title.textContent}:</b> ${review}`;
+  ul.appendChild(li);
+  const input = document.querySelector("input");
+  input.value = "";
+  form.reset();
+});
 
+getMovies();
